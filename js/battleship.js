c434d14ceb25a6b462acbafd41056b2871d06586
Jose Diaz <Diaz.M.Jose68@gmail.com>
2021-11-03 21:26:15 -0500

    Completed 1st part into battleship js code

--- js/battleship.js
+++ js/battleship.js
@@ -8,12 +8,22 @@ var hits = 0;
 var guesses = 0;
 var isSunk = false;
 
-while(isSunk){
-    guess = parseFloat(prompt("Where to Captain!(enter number between 0-6)"));
-    if(guess != location1 || guess != location2 || guess != location3){
-        prompt("Miss! ready to fire!(choose number)");
-    }else{
+while(isSunk == false) {
+    guess = prompt("Where to Captain!(enter number between 0-6)");
+    if (guess < 0 || guess > 6) {
+        alert("Enter a valid number");
+    } else {
+        guesses = guesses + 1;
+        if (guess == location1 || guess == location2 || guess == location3) {
+            hits = hits + 1;
 
+            if (hits == 3) {
+                isSunk = true;
+                alert("You sunk my Battleship!!!");
+            }
+        }
     }
 }
+var stats = "You took " + guesses + " guesses to sink the battleship, which means your shooting accuracy was " + (3/guesses);
+alert(stats);
 
